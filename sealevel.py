da2307677de2dfee8d2968024ec2500d989a296c
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-06-29 10:06:34 -0700

    Update sealevel.py

--- sealevel.py
+++ sealevel.py
@@ -45,6 +45,8 @@ fig = go.Figure(data=[go.Surface(z=st.session_state.elevation_data, x=st.session
 #fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, mllw), showscale=False, opacity=0.5, colorscale=[[0, 'blue'], [1, 'blue']], name='MLLW'))
 #fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, mhhw), showscale=False, opacity=0.5, colorscale=[[0, 'red'], [1, 'red']], name='MHHW'))
 #fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, max_tide), showscale=False, opacity=0.5, colorscale=[[0, 'green'], [1, 'green']], name='Max Tide'))
-fig.update_layout(title='Marine Terminal Elevation with Tidal Levels', autosize=True, scene=dict(zaxis=dict(title='Elevation (feet)')), margin=dict(l=65, r=50, b=65, t=90))
+if st.checkbox("Show MLLW"):
+    fig.add_trace(go.Surface(z=np.full(data.shape, mllw), showscale=False, opacity=0.5, colorscale=[[0, 'blue'], [1, 'blue']], name='MLLW'))
+fig.update_layout(title='Marine Terminal Elevation with Tidal Levels', autosize=False, scene=dict(zaxis=dict(title='Elevation (feet)')), margin=dict(l=65, r=50, b=65, t=90))
 
 st.plotly_chart(fig, use_container_width=True)
