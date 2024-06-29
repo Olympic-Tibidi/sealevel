c6155be8901c14551810d6b53c3ea5bcb91a4ca9
Olympic-Tibidi <77189669+Olympic-Tibidi@users.noreply.github.com>
2024-06-29 09:02:59 -0700

    Update sealevel.py

--- sealevel.py
+++ sealevel.py
@@ -67,9 +67,7 @@ max_tide = st.slider('Max Tide Level', -3.0, 20.0, 0.1) - 4.43  # Adjusted for M
 if 'fig' not in st.session_state:
     st.session_state.fig = go.Figure()
     st.session_state.fig.add_trace(go.Surface(z=st.session_state.elevation_data, x=st.session_state.lons, y=st.session_state.lats, colorscale='Earth', name='Elevation'))
-    st.session_state.fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, mllw), showscale=False, opacity=1, colorscale=[[0, 'blue'], [1, 'blue']]))
-    st.session_state.fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, mhhw), showscale=False, opacity=0.5, colorscale=[[0, 'red'], [1, 'red']]))
-    st.session_state.fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, maxtide), showscale=False, opacity=0.5, colorscale=[[0, 'green'], [1, 'green']]))
+    
     st.session_state.fig.update_layout(
          title='Marine Terminal Ewdwdwlevation with Tidal Levels',
     autosize=False,  # Disable autosizing to set custom width and height
@@ -82,6 +80,8 @@ if 'fig' not in st.session_state:
 # st.session_state.fig.for_each_trace(
 #     lambda trace: trace.update(z=np.full(st.session_state.elevation_data.shape, max_tide)) if trace.name == 'Max Tide' else ()
 # )
-
+st.session_state.fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, mllw), showscale=False, opacity=1, colorscale=[[0, 'blue'], [1, 'blue']]))
+st.session_state.fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, mhhw), showscale=False, opacity=0.5, colorscale=[[0, 'red'], [1, 'red']]))
+st.session_state.fig.add_trace(go.Surface(z=np.full(st.session_state.elevation_data.shape, maxtide), showscale=False, opacity=0.5, colorscale=[[0, 'green'], [1, 'green']]))
 # Plot the figure
 st.plotly_chart(st.session_state.fig)
